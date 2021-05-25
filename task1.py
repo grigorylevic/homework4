9f6d4ab59fafe95fe336e0bc629c7478baa79379
grigorylevic <69089107+grigorylevic@users.noreply.github.com>
2021-05-25 20:48:14 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,11 @@
+"""
+The Guessing Game.
+
+Write a program that generates a random number between 1 and 10 and lets the user guess what number was generated. The result should be sent back to the user via a print statement.
+
+"""
+
+
 import random
 
 user_number = int(input("Enter your number: "))
