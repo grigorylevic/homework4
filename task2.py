6c76f35db74ae00a4102ee6ee39ffc038c3c0a4b
grigorylevic <69089107+grigorylevic@users.noreply.github.com>
2021-05-25 20:48:45 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,3 +1,12 @@
+"""
+The birthday greeting program.
+
+Write a program that takes your name as input, and then your age as input and greets you with the following:
+
+“Hello <name>, on your next birthday you’ll be <age+1> years”   
+
+"""
+
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print("Hello ",name,", on your next birthday you’ll be " ,str(age + 1), " years")
